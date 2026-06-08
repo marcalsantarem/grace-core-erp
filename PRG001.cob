a885de85e0584b79c6214644122b831c272299c7
marcal santarem <marcal.santarem-terceiro@authsoft.com.br>
2026-06-08 09:27:55 -0300

    Melhorado o programa de menu inicial. Centralizadas algumas rotinas úteis para impressão de tela em um copy separado.

--- PRG001.cob
+++ PRG001.cob
@@ -1,37 +1,36 @@
+      *>---------------------------------------------------------------*
+      *> PRG001.COB - PROGRAMA PRINCIPAL                               *
+      *> Menu de acesso aos modulos do sistema                         *
+      *>---------------------------------------------------------------*
        IDENTIFICATION DIVISION.
        PROGRAM-ID. PRG001.
        AUTHOR. MARCAL SANTAREM.
-      *================================================================*
+      *>---------------------------------------------------------------*
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SPECIAL-NAMES.
            DECIMAL-POINT IS COMMA.
-      *================================================================*
+      *>---------------------------------------------------------------*
        DATA DIVISION.
        FILE SECTION.
-      *================================================================*
+      *>---------------------------------------------------------------*
        WORKING-STORAGE SECTION.
        01 WS-OPCAO             PIC X(1)  VALUE SPACES.
        01 WS-CONTINUAR         PIC X(1)  VALUE 'S'.
-      *================================================================*
+      *>---------------------------------------------------------------*
        PROCEDURE DIVISION.
       *----------------------------------------------------------------*
        INICIO.
-           DISPLAY "Menu Principal do Sistema"
            PERFORM MENU-PRINCIPAL UNTIL WS-CONTINUAR = 'N'
            STOP RUN.
-      *================================================================*
-      * MENU PRINCIPAL                                                 *
-      *================================================================*
+      *>---------------------------------------------------------------*
+      *> MENU PRINCIPAL                                                *
+      *>---------------------------------------------------------------*
        MENU-PRINCIPAL.
-           CALL "SYSTEM" USING "cls"
-           DISPLAY "  1 - Módulo 1"
-           DISPLAY " "
-           DISPLAY "  0 - Sair"
-           DISPLAY " "
+           PERFORM                IMPRIME-MENU.        
            DISPLAY "Opcao: " WITH NO ADVANCING
            ACCEPT WS-OPCAO
-
+           
            EVALUATE WS-OPCAO
               WHEN "1"
                  DISPLAY "Modulo 1 selecionado."
@@ -41,5 +40,25 @@
               WHEN OTHER
                  DISPLAY "Opcao invalida."
            END-EVALUATE.
-           cALL "SYSTEM" USING "PAUSE".
-      *================================================================*
+           CALL "SYSTEM" USING "PAUSE".
+      *>---------------------------------------------------------------*
+       IMPRIME-MENU.
+           CALL "SYSTEM" USING "cls"
+           PERFORM                LINHA-TRACEJADA
+           PERFORM                LINHA-EM-BRANCO
+           DISPLAY "-                  Bem Vindo ao                  -"
+           DISPLAY "-                 Grace Core ERP                 -"
+           PERFORM                LINHA-EM-BRANCO
+           PERFORM                LINHA-TRACEJADA
+
+           PERFORM                LINHA-VAZIA
+           DISPLAY "- Selecione uma opcao do menu:"
+           PERFORM                LINHA-VAZIA
+           DISPLAY "  1 - Módulo 1"
+           PERFORM                LINHA-VAZIA
+           DISPLAY "  0 - Sair"
+           PERFORM                LINHA-VAZIA.     
+      *>---------------------------------------------------------------*
+           COPY                   TELAUTIL.cpy.
+      *>---------------------------------------------------------------*
+      
